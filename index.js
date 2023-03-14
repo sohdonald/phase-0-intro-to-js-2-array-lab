a71a264c12a447bfbe184e199dddb3b3e42b792c
Donald Soh <sohdonald@gmail.com>
2023-03-14 16:49:20 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+// 1 pass, 8 to go
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+// 2 pass, 7 to go
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+// 3 down, 6 left
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+// 4 pass, 5 to go
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+// 5 down, 4 left
+
+function appendCat(name) {
+ //   cats.push(name);
+ return [...cats, name];
+ }
+ // 6 down, 3 left
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+//7 down, 2 left
+
+function removeLastCat() {
+    return cats.slice(0, cats.length-1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
